3f2ec4a6f0580cdb54778e3f37f6f729471c76df
junaid-h0ssain <158271661+junaid-h0ssain@users.noreply.github.com>
2025-06-03 19:18:09 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Date;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello and welcome!");
+        System.out.println("\nnice dick");
+        Date now = new Date();
+        System.out.println(now);
+    }
+}
